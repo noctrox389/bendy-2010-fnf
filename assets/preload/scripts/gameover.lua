c20ace73c54fee847756e7b8658a5d238bcd21c8
noctrox389 <114263655+noctrox389@users.noreply.github.com>
2022-11-16 19:41:03 -0400

    Add files via upload

---
+++ assets/preload/scripts/gameover.lua
@@ -0,0 +1,6 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bf-ink');
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', '');
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver');
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd');
+end	
